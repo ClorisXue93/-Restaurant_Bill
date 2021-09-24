ec4e1d5076e6e8e73404948d410e65eb955889a1
Chen Xue <cxue@cpp.edu>
2021-09-24 01:13:56 -0700

    add source file

---
+++ source.c
@@ -0,0 +1,6 @@
+/*this is the source comment file for the first program--restaurant bill*/
+/*first, I need a main function with two arguments which are tax and tip percentages*/
+/*secondly, I need a function to calculate tax, one function calculate tips*/
+/*thirdly, I need a function contains all four kinds of food with their price*/
+/*then, I need a function which picks food ramdonly from the food options*/
+/*lastly, I need a function to calculate all fees*/
