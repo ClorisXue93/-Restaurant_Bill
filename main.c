42204490e8746fae5190e7f9af41f8c2f5156fcc
Chen <89332989+ClorisXue93@users.noreply.github.com>
2021-09-24 03:47:20 -0700

    second main

--- main.c
+++ main.c
@@ -2,62 +2,55 @@
 #include <stdlib.h>
 #include <time.h>
 
-double tax_function(double tax, double fee) {
-    return (fee * tax / 100);
+void game(int max, int num) {
+    int guess;
+    printf("Please enter a number from 1-%i: ", max);
+    scanf("%i", &guess);
+    while (guess != num) {
+        if (guess > num) {
+            printf("The number you entered is too high.\n");
+        }
+        else
+        {
+            printf("The number you entered is too low.\n");
+
+        }
+        printf("Please enter again: ");
+        scanf("%i", &guess);
+    }
+    printf("The number is %i, YOU WIN!!!\n", num);
 }
 
-double tips_function(double tips, double fee) {
-    return (fee * tips / 100);
-}
-
-char* meal_select() {
+int getNum(int max) {
     srand(time(NULL));
-    int rd = rand() % 4;
-    if (rd == 0)
-    {
-        return "9.95";
-    }
-    else if (rd == 1)
-    {
-        return "4.55";
-    }
-    else if (rd == 2)
-    {
-        return "13.25";
-    }
-    else if (rd == 3)
-    {
-        return "22.35";
-    }
+    return rand() % max + 1;
 }
 
-const char* meal_name(double fee) {
-    if (fee == 9.95)
-        return "Salad";
-    else if (fee == 4.55)
-        return "Soup";
-    else if (fee == 13.25)
-        return "Sandwich";
-    else if (fee == 22.35)
-        return "Pizza";
-}
-
-int main (int argc, char* argv[]) {
-    double tax, tips, fee, total;
-    fee = 0;
-    total = 0;
-    char *ptr;
-    fee = strtod(meal_select(), &ptr);
-    printf("fee is %f.\n", fee);
-    tax = strtod(argv[1], &ptr);
-    // sscanf(argv[1], "%f", &tax);
-    tips = strtod(argv[2], &ptr);
-    // sscanf(argv[2], "%f", &tips);
-    tax = tax_function(tax, fee);
-    tips = tips_function(tips, fee);
-    printf("The item is %s.\n", meal_name(fee));
-    printf("The price for the meal is: %.2f.\n", fee);
-    printf("The tax is: %.2f.\n", tax);
-    printf("The tips is: %.2f.\n", tips);
-    printf("The total is: %.2f.\n", fee + tax + tips);
+int main() {
+    int number;
+    int max = 10;
+    do {
+        printf("Press 1 to play a game.\nPress 2 to change the max number.\nPress 3 to quit.\n");
+        printf("Please enter a number: \n");
+        scanf("%i", &number);
+        if (number > 3 || number < 1)
+        {
+            printf("You entered wrong number, please enter again: ");
+        }
+        else if (number == 2)
+        {
+            printf("Enter a number to change a max value: ");
+            scanf("%i", &max);
+            while(max <= 0)
+            {
+            printf("You cannot enter a nagetive number. \nEnter a number to change a max value: ");
+            scanf("%i", &max);
+            }
+        }
+        else if (number == 1)
+        {
+            game(max, getNum(max));
+        }
+    } while (number != 3);
+    printf("Game over!");
 }
